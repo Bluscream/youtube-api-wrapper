7a11725c8b416b56368715c00999f38106e4a6aa
Bluscream <Bluscream@users.noreply.github.com>
2024-08-30 21:58:02 +0200

    a

--- app.py
+++ app.py
@@ -1,28 +1,41 @@
-from flask import Flask, request, jsonify
-import requests
-from concurrent.futures import ThreadPoolExecutor, as_completed
-import json
-import time
+import time, json, requests
 
-app = Flask(__name__)
+from flask import Flask, request, jsonify
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from threading import Thread
+
+
+
 
 # Hardcoded list of API endpoints
 API_ENDPOINTS = {
-    "youtube-data": "https://yt-dl.org/api/v2.json",
-    "youtube-dislike": "https://dislike.yt/api/v1/video",
-    "youtube-sponsorblock": "https://sponsor.ajay.app/api/skipSegments",
-    "youtube-dearrow": "https://dearrows.com/api/v1/video"
+    "youtube-data": "https://yt.lemnoslife.com/noKey/videos?part=statistics&id={ytid}",
+    "youtube-dislike": "https://returnyoutubedislikeapi.com/votes?videoId={ytid}",
+    "youtube-sponsorblock": "https://sponsor.ajay.app/api/skipSegments?videoID={ytid}",
+    "youtube-dearrow": "https://sponsor.ajay.app/api/branding?videoID={ytid}"
 }
 
-def fetch_data(endpoint, yt_video_id):
-    try:
-        if endpoint == "youtube-data":
-            url = f"{API_ENDPOINTS[endpoint]}?id={yt_video_id}"
-        elif endpoint in ["youtube-dislike", "youtube-sponsorblock"]:
-            url = f"{API_ENDPOINTS[endpoint]}?videoId={yt_video_id}"
-        else:
-            url = f"{API_ENDPOINTS[endpoint]}?videoID={yt_video_id}"
+class Cache(Thread):
+    data = {} # Simple cache dictionary
+    def __init__(self, interval=300):  # 5 minutes interval
+        super().__init__()
+        self.interval = interval
+        self.daemon = True  # Set as daemon so it exits when main program finishes
+        self.run()
 
+    def run(self):
+        while True:
+            time.sleep(self.interval)
+            self.data = {}
+            print("Cache cleaned")
+
+def is_youtube_video_id(id: str): return id and len(id) == 11 and id.startswith(('3', '9'))
+
+def fetch_data(endpoint, yt_video_id: str):
+    try:
+        # String format the videoId into the API URL
+        url = API_ENDPOINTS[endpoint].format(ytid=yt_video_id)
+        
         response = requests.get(url)
         response.raise_for_status()
         return response.json()
@@ -30,19 +43,27 @@ def fetch_data(endpoint, yt_video_id):
         print(f"Error fetching {endpoint}: {str(e)}")
         return None
 
-@app.route('/combine', methods=['POST'])
+cache = Cache(interval=300)
+
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
 def combine_responses():
     start_time = time.time()
 
     # Get the YouTube video ID from the request body
-    data = request.json
-    yt_video_id = data.get('videoId')
+    yt_video_id = request.args.get('videoId')
 
-    if not yt_video_id:
-        return jsonify({"error": "Missing 'videoId' in the request"}), 400
+    # Validate the video ID
+    # if not is_youtube_video_id(yt_video_id): return jsonify({"error": "missing or invalid 'videoId'"}), 400
 
-    # Create a dictionary to store the fetched data
-    combined_response = {}
+    combined_response = {} # Create a dictionary to store the fetched data
+
+    # Check cache first
+    if yt_video_id in cache.data:
+        print(f"Cached response found for {yt_video_id}")
+        combined_response["time"] = time.time() - start_time
+        combined_response.update(cache.data.get(yt_video_id))
+        return jsonify(combined_response)
 
     # Use ThreadPoolExecutor to make concurrent requests
     with ThreadPoolExecutor(max_workers=len(API_ENDPOINTS)) as executor:
@@ -58,17 +79,15 @@ def combine_responses():
                 print(f"Fetching {endpoint} generated an exception: {exc}")
 
     # Calculate the total execution time
-    end_time = time.time()
-    total_time = end_time - start_time
-
-    # Check if any of the requests timed out
-    if total_time > 5:
-        return jsonify({"error": "Request timed out after 5 seconds"}), 408
+    combined_response["time"] = time.time() - start_time
 
     # Combine the responses
     combined_response = {k: v for k, v in combined_response.items() if v}
 
+    # Cache the response
+    cache.data[yt_video_id] = combined_response
+
     return jsonify(combined_response)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
