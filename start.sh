1527289299cadf8aaff1035c242b537b030637e3
Bluscream <Bluscream@users.noreply.github.com>
2024-09-01 04:52:41 +0200

    s

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 # git clone https://github.com/Bluscream/youtube-api-wrapper
-git pull
+git pull && git submodule init && git submodule update && git submodule status
 python -m venv .venv
 .venv/bin/pip install -r requirements.txt
 .venv/bin/python app.py
